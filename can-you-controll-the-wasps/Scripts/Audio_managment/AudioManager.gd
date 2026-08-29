f74900594f0e113af2bf1ac114a483d65822b1fc
marekkozel <play.majko612@gmail.com>
2026-08-29 21:04:57 +0200

    Final_touches, Export_settings, Audio_fix

--- can-you-controll-the-wasps/Scripts/Audio_managment/AudioManager.gd
+++ can-you-controll-the-wasps/Scripts/Audio_managment/AudioManager.gd
@@ -1,15 +1,8 @@
 extends Node2D
-## Audio manager node. Inteded to be globally loaded as a 2D Scene. Handles [method create_2d_audio_at_location()] and [method create_audio()] to handle the playback and culling of simultaneous sound effects.
-##
-## To properly use, define [enum SoundEffect.SOUND_EFFECT_TYPE] for each unique sound effect, create a Node2D scene for this AudioManager script add those SoundEffect resources to this globally loaded script's [member sound_effects], and setup your individual SoundEffect resources. Then, use [method create_2d_audio_at_location()] and [method create_audio()] to play those sound effects either at a specific location or globally.
-## 
-## See https://github.com/Aarimous/AudioManager for more information.
-##
-## @tutorial: https://www.youtube.com/watch?v=Egf2jgET3nQ
 
-var sound_effect_dict: Dictionary = {} ## Loads all registered SoundEffects on ready as a reference.
+var sound_effect_dict: Dictionary = {}
 
-@export var sound_effects: Array[SoundEffect] ## Stores all possible SoundEffects that can be played.
+@export var sound_effects: Array[SoundEffect]
 
 
 func _ready() -> void:
